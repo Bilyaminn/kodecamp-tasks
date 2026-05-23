f77e709c051858f6cb007cbc9d6f70b3037cff6e
Sani Bilyaminu <sani.bilyaminn@gmail.com>
2026-05-23 11:34:01 +0100

    Add a program that accepts a string in kabeb case and returns the string in camel case.

---
+++ task2/convertToCamelCase.js
@@ -0,0 +1,11 @@
+const convertToCamelCase = str => {
+    const word = str.split("-")
+
+    for(let i = 1; i < word.length; i++) {
+        word[i] = word[i][0].toUpperCase() + word[i].slice(1)  
+    }
+    
+    return word.join("")
+}
+
+console.log(convertToCamelCase('hello-there'))
